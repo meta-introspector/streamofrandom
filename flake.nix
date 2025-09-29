b4364056cddcccc1e0fe8de084a68d4097491889
mike <h@solfunmeme.com>
2025-09-29 20:38:16 +0000

    feat: Update streamofrandom flake.nix with pre-commit tools and meta-introspector inputs

--- flake.nix
+++ flake.nix
@@ -2,11 +2,12 @@
   description = "A basic flake for streamofrandom";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:meta-introspector/nixpkgs?ref=feature/CRQ-016-nixify";
+    flake-utils.url = "github:meta-introspector/flake-utils?ref=feature/CRQ-016-nixify";
+    vendoredLintStaged.url = "github:meta-introspector/time-2025?ref=feature/CRQ-016-nixify&dir=/09/27/7-concepts/1-build-system";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, vendoredLintStaged }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
@@ -18,6 +19,14 @@
             pkgs.nix
             pkgs.bash
             pkgs.coreutils
+            pkgs.pre-commit
+            pkgs.nixpkgs-fmt
+            pkgs.statix
+            pkgs.shellcheck
+            pkgs.nodePackages."@commitlint/cli"
+            pkgs.vale
+            pkgs.nodejs
+            vendoredLintStaged.packages.${system}.vendoredLintStaged
           ];
         };
       }
